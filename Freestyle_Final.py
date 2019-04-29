47fac5e09986b4b89795f50668e8462537d4cda8
sambeyda <46543631+sambeyda@users.noreply.github.com>
2019-04-29 11:47:23 -0400

    Plugged in total points scored algorithm (also fixed BeautifulSoup warning)

--- Freestyle_Final.py
+++ Freestyle_Final.py
@@ -3,6 +3,7 @@
 
 from bs4 import BeautifulSoup
 import requests
+import statistics
 #import pandas as pd
 
 #
@@ -15,7 +16,7 @@ print("------------------")
 ###Will need to put data validation to make sure user inputs valid teams!!!!
 User_team1=input("Please enter team 1: ")
 User_team2=input("Please enter team 2: ")
-print("GETTING HTML CONTENTS...")
+print("SCRAPING DATA")
 #
 ##SCRAPING KENPOM DATA 
 #
@@ -24,7 +25,7 @@ print("GETTING HTML CONTENTS...")
 base_filepath = 'http://kenpom.com/index.php' #changed professor's filepath
 #Use Requests package to get contents of page
 raw_data = requests.get(base_filepath)
-soup=BeautifulSoup(raw_data.text)
+soup=BeautifulSoup(raw_data.text, "lxml") #Gets rid of Beautiful Soup error
 ratings_table = soup.find("table", id="ratings-table")
 
 print("RATINGS TABLE", type(ratings_table))
@@ -62,28 +63,27 @@ for row in rows:
     except IndexError as e:
         print("")
         #breakpoint()
-        # looks like offending rows include:
-        # <tr class="thead1">
-        #   <th class="hard_left"></th>
-        #   <th class="next_left"></th>
-        #   <th colspan="3"></th>
-        #   <th class="divide" colspan="4">
-        #   </th><th class="divide" colspan="2">
-        #   </th><th class="divide" colspan="2"></th>
-        #   <th class="divide" colspan="6">Strength of Schedule</th>
-        #   <th class="divide" colspan="2">NCSOS</th>
-        # </tr>
-        # hoefully we can just skip them
-        #next()
+ 
+##
+#Matching User Input to Team in List
+##
 matching_team1 = [t for t in teams if t["Name"]==User_team1]
 matching_team2 = [t for t in teams if t["Name"]==User_team2]
-matching_team1_tempo = matching_team1[0]["Tempo"]
-matching_team2_tempo = matching_team1[0]["Tempo"]
+matching_team1_tempo = matching_team1[0]["Tempo"] #Team1 tempo
+matching_team2_tempo = matching_team2[0]["Tempo"] #Team2 tempo
 
+#Efficiency factor: How well Team1's offense will perform against Team2's defense
 matching_team1_efficiency_adv = statistics.mean([matching_team1[0]["AdjO"],matching_team2[0]["AdjD"]])
-matching_team1_std=(matching_team1_efficiency_adv/100)
+matching_team1_std=(matching_team1_efficiency_adv/100) #Standardize per 100 possesions
 matching_team2_efficiency_adv = statistics.mean([matching_team2[0]["AdjO"],matching_team1[0]["AdjD"]])
-matching_team2_std=(matching_team2_efficiency_adv/100
+matching_team2_std=(matching_team2_efficiency_adv/100) #Standardize per 100 possesions
 
-#print(matching_team1[0]["Name"])
-#print(matching_team2[0]["AdjO"])
+##
+# TOTAL POINTS CALCULATION
+## Average of teams standard efficiency advantage (points per 100 possesions) multiplied by tempo (how many possesions)
+total_efficiency_factor = statistics.mean([matching_team1_std,matching_team2_std])*2
+Average_tempo=statistics.mean([matching_team1_tempo,matching_team2_tempo])
+Final_score=total_efficiency_factor*Average_tempo
+print("--------------------")
+print("ALGORITHM SUCCESSFUL")
+print(f"The total predicted amount of points scored in a hypothetical game between {User_team1} and {User_team2} is: {Final_score}")
