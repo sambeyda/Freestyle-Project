3065ba63fdda00ddbb1dd32ebff865d1273bcb7b
sambeyda <46543631+sambeyda@users.noreply.github.com>
2019-04-30 17:52:10 -0500

    Update pytest.py

--- pytest.py
+++ pytest.py
@@ -4,11 +4,11 @@
 #pytest to determine if algorithm ran correctly
 #This test checks if our model spits out any extreme values
 
-def func (Final_score):
+def Final_score():
     return (Final_score)
 
 def test_max():
-    assert func(Final_score) < 200
+    assert Final_score < 200
 
 def test_min():
-    assert func(Final_score) > 50
+    assert Final_score > 50
